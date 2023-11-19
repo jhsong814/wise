f833225d9082ff0fbe97964461daa1a4d376a126
jhs <jhsong814@gmail.com>
2023-11-19 16:14:06 +0900

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("hello world");
+
+
+    }
+}
